eb4232298f84a6a91660f52812260d818beac930
Sean Smith <wwsportsguy@gmail.com>
2012-03-21 18:06:36 -0400

    now the arduino has the ability to output the status of the door to a website, one part down, many more to go

--- garage_door/garage_door.ino
+++ garage_door/garage_door.ino
@@ -1,101 +1,121 @@
- /* Ultrasound Sensor
- *------------------
- *
- * Reads values (00014-01199) from an ultrasound sensor (3m sensor)
- * and writes the values to the serialport.
- *
- * http://www.xlab.se | http://www.0j0.org
- * copyleft 2005 Mackie for XLAB | DojoDave for DojoCorp
- *
- */
+//these imports allow us to use the ethernet shield
+#include "SPI.h"
+#include "Ethernet.h"
+#include "WebServer.h"
 
-const byte ultraSoundSignal = 7; // Ultrasound signal pin
+/*
+*Below are the global variables and constants
+*/
+static uint8_t mac[] = { 0x90, 0xA2, 0xDA, 0x0D, 0x08, 0x0C };
+static uint8_t ip[] = { 192, 168, 1, 20 };
+#define PREFIX ""
+WebServer webserver(PREFIX, 80);
+const byte ULTRASOUNDSIGNAL = 7; // Ultrasound signal pin
 int val = 0;
 int timecount = 0; // Echo counter
-int threshold = 250;
+const int THRESHOLD = 250;
 boolean isOpen = false;
 boolean wasOpen = false;
-const byte openPin = 9;
-const byte doorPin = 5;
+const byte OPENPIN = 9;
+const byte DOORPIN = 5;
 unsigned long timeOpen = 0;
-unsigned const long autoClose = 0;
-
-void setup() {
-  pinMode(openPin, OUTPUT);  //the pin with the LED for notification
-  attachInterrupt(0, changeDoorState, RISING);  //located on digital pin 2
-  pinMode(doorPin, OUTPUT);
-  digitalWrite(doorPin, LOW);
-}
-
-void loop() {
-  readDoorSensor();
-  delay(100);
-}
+unsigned const long AUTOCLOSE = 0;
 
 void readDoorSensor(){
   timecount = 0;
   val = 0;
-  pinMode(ultraSoundSignal, OUTPUT); // Switch signalpin to output
+  pinMode(ULTRASOUNDSIGNAL, OUTPUT); // Switch signalpin to output
 
   /* Send low-high-low pulse to activate the trigger pulse of the sensor
    * -------------------------------------------------------------------
    */
 
-  digitalWrite(ultraSoundSignal, LOW); // Send low pulse
+  digitalWrite(ULTRASOUNDSIGNAL, LOW); // Send low pulse
   delayMicroseconds(2); // Wait for 2 microseconds
-  digitalWrite(ultraSoundSignal, HIGH); // Send high pulse
+  digitalWrite(ULTRASOUNDSIGNAL, HIGH); // Send high pulse
   delayMicroseconds(5); // Wait for 5 microseconds
-  digitalWrite(ultraSoundSignal, LOW); // Holdoff
+  digitalWrite(ULTRASOUNDSIGNAL, LOW); // Holdoff
 
   /* Listening for echo pulse
    * -------------------------------------------------------------------
    */
 
-  pinMode(ultraSoundSignal, INPUT); // Switch signalpin to input
-  val = digitalRead(ultraSoundSignal); // Append signal value to val
+  pinMode(ULTRASOUNDSIGNAL, INPUT); // Switch signalpin to input
+  val = digitalRead(ULTRASOUNDSIGNAL); // Append signal value to val
   while(val == LOW) { // Loop until pin reads a high value
-    val = digitalRead(ultraSoundSignal);
+    val = digitalRead(ULTRASOUNDSIGNAL);
   }
 
   while(val == HIGH) { // Loop until pin reads a high value
-    val = digitalRead(ultraSoundSignal);
+    val = digitalRead(ULTRASOUNDSIGNAL);
     timecount = timecount +1;            // Count echo pulse time
   }
 
   /* Writing out values to the serial port
    * -------------------------------------------------------------------
    */
-  if(timecount > threshold){  //we are closed
+  if(timecount > THRESHOLD){  //we are closed
     if(wasOpen){
       wasOpen = false;
     }
     isOpen = false;
-    digitalWrite(openPin, LOW); //turn light off the LED if it's open
+    digitalWrite(OPENPIN, LOW); //turn light off the LED if it's open
   }
   else{  //we are open
     if(wasOpen == false){
       timeOpen = millis();
       wasOpen = true;
     }else{
-      if(timeOpen+autoClose < millis()){
+      if(timeOpen+AUTOCLOSE < millis()){
         changeDoorState();
       }
     }
     isOpen = true;
-    digitalWrite(openPin, HIGH);  //light up the LED if it's open
+    digitalWrite(OPENPIN, HIGH);  //light up the LED if it's open
   }
 
   /* Delay of program
    * -------------------------------------------------------------------
    */
-  delay(10000); 
 }
 
 /*This should get called if the button is pressed in order to close 
  *the garrage door using an interrupt.
 */
 void changeDoorState(){
-  digitalWrite(doorPin, HIGH);
+  digitalWrite(DOORPIN, HIGH);
   delay(250);
-  digitalWrite(doorPin, LOW);
+  digitalWrite(DOORPIN, LOW);
+}
+
+void statusCommand(WebServer &server, WebServer::ConnectionType type, char *, bool){
+  server.httpSuccess(); //send the 200 saying we're good
+  if(type !=WebServer::HEAD){
+    P(DoorStatus) = "<h1>Door Status: ";
+    server.printP(DoorStatus);
+    if(isOpen){
+      server.print("Open </h1><br />");
+    }else{
+      server.print("Closed </h1><br />");
+    }
+  }
+}
+
+void setup() {
+  pinMode(OPENPIN, OUTPUT);  //the pin with the LED for notification
+  attachInterrupt(0, changeDoorState, RISING);  //located on digital pin 2
+  pinMode(DOORPIN, OUTPUT);
+  digitalWrite(DOORPIN, LOW);
+  Ethernet.begin(mac, ip);
+  webserver.setDefaultCommand(&statusCommand);
+  webserver.addCommand("index.html", &statusCommand);
+  webserver.begin();
+}
+
+void loop() {
+  char buff[64];
+  int len = 64;
+  readDoorSensor();
+  webserver.processConnection(buff, &len);
+  delay(100);
 }
